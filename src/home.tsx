3ca6f5cdae67afa3b3c1091db8ed0fd7492d7c82
Stefano Canducci <stefano@canducci.org>
2025-08-24 11:51:08 +0200

    some clean up, added mui and fixtures data

---
+++ src/home.tsx
@@ -0,0 +1,16 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <>
+      <Button variant="outlined">Hello!</Button>
+    </>
+  );
+};
+
+export default App;
